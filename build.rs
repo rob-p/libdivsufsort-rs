31b1bdc3bd4da8f706c0cda858f17885580eb8a3
Rob Patro <rob-p@github.com>
2023-11-08 15:03:28 -0500

    try other build flags

--- build.rs
+++ build.rs
@@ -8,11 +8,12 @@ fn main() {
     // cmake build
     let dst = Config::new("src/libdivsufsort")
         .define("CMAKE_BUILD_TYPE", "Release")
-        .define("CMAKE_C_FLAGS", "-O3")
         .define("BUILD_EXAMPLES", "OFF")
         .define("BUILD_SHARED_LIBS", "OFF")
         .define("BUILD_DIVSUFSORT64", "ON")
         .define("CMAKE_INSTALL_LIBDIR", out_path)
+        .cflag("-O3")
+        .cflag("-mcx16")
         .build();
     println!("cargo:rustc-link-search=native={}", dst.display());
     println!("cargo:rustc-link-lib=static=divsufsort");
